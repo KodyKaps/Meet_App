95c828cff7fc19494c48731e13776e20656f6fd2
Kody Kapetanis <kodykapetanis@yahoo.com>
2025-01-28 06:29:11 -0500

    Event list compnents

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 // src/App.jsx
-import React from 'react';
-import './App.css'
-
-import EventList from './components/eventList';
+import React, { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
+import EventList from './components/eventList';
 import NumberOfEvents from './components/NumberOfEvents';
-const App = () => {
-  return (
-    <div className="App">
-      <CitySearch/>
-      <NumberOfEvents/>
-      <EventList />
-    </div>
-  );
+import { getEvents } from './api';
+import './App.css';
+
+
+const [events, setEvents] = useState([]);
+const [currentNOE, setCurrentNOE] = useState(32);
+
+return (
+  <div className="App">
+    <CitySearch />
+    <NumberOfEvents />
+    <EventList events={events} />
+  </div>
+);
+
+const fetchData = async () => {
+  const allEvents = await getEvents();
+  setEvents(allEvents.slice(0, currentNOE));
 }
+
+useEffect(() => {
+  fetchData();
+}, []);
+
 
 
 export default App;
