372a043968f4be1afb54f67c461722cb3474fdd3
Kody Kapetanis <kodykapetanis@yahoo.com>
2025-02-05 05:36:26 -0500

    Clicking Details button

--- e2etests/EndToEnd.test.js
+++ e2etests/EndToEnd.test.js
@@ -37,4 +37,20 @@ describe('show/hide an event details', () => {
         const eventDetails = await page.$('.event .details');
         expect(eventDetails).toBeNull();
       });
+
+      //Clicking Details button
+      test('User can expand an event to see its details', async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:5173/');
+    
+        await page.waitForSelector('.event');
+        await page.click('.event .details-btn');
+    
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeDefined();
+        await browser.close();
+      });
+      
 });
+
