1d5145d2d114c181c360ecae79767d0aece5eedd
Kody Kapetanis <kodykapetanis@yahoo.com>
2025-02-10 20:38:37 -0500

    deploy to test pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import * as atatus from 'atatus-spa';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 atatus.config('de4221f150714c519f79f33e157eb54b').install();
 
 createRoot(document.getElementById('root')).render(
@@ -10,3 +13,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+serviceWorkerRegistration.register();
